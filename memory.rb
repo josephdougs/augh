4304b1e17ec1cfee1716b45a57cca64a1984c248
Joey Smith <joey@ivysoftworks.com>
2016-10-15 01:43:04 -0700

    Variables are created and deleted

--- memory.rb
+++ memory.rb
@@ -1,11 +1,28 @@
+require 'thread'
+
 # keeps track of variables
 
+# variables live for fifteen minutes
+VARIABLE_LIFETIME = 60 * 15
+
+
 class Memory
-    attr_accessor :vars
-    
     # TODO: make it a singleton or something like that?
     def initialize
-        vars = {}
+        @vars = {}
+
+        Thread.new do
+            while true
+                # loop through vars and delete ones
+                # that have been around too long
+                @vars.each do |name, var|
+                    now = Time.now.to_i
+                    if now >= var.kill_time
+                        @vars.delete(name)
+                    end
+                end
+            end
+        end
     end
 
     def new_var(name, value)
@@ -13,32 +30,28 @@ class Memory
             bad_name_fail
         end
 
-        if !value.is_a?(Type)
-            bad_value_fail
-        end
+        @vars[name] = Var.new(value)
     end
 
     def bad_name_fail
         puts "Given name is not valid"
         exit 1
     end
-    
+end
+
+class Var
+    attr_reader :val, :kill_time
+    def new(val)
+        if !value.is_a?(Type)
+            bad_value_fail
+        end
+
+        @val = val
+        @time_created = Time.now.to_i + VARIABLE_LIFETIME
+    end
+
     def bad_value_fail
         puts "Given value is not valid"
         exit 1
     end
-
 end
-
-class Var
-    attr_reader :val
-
-    def initialize
-        if !value.is_a?(Type)
-            bad_value_fail
-        end
-        
-    end
-    
-    
-    end
