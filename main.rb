345cf238ea187beb7a4401c53ef629c568977974
Joey Smith <joey@ivysoftworks.com>
2016-10-17 18:53:39 -0700

    More work

--- main.rb
+++ main.rb
@@ -1,17 +1,49 @@
+require 'set'
 require './preprocess.rb'
+
+# the set of special keywords
+KEYWORDS = Set.new ["here", "go", "int",
+    "bool", "string", "yep", "nope", "embiggen",
+    "print", "augh"]
+
+
+# this class tokenizes each line
+class Tokenizer
+    def initialize(line)
+        line_arr = split_line(line)
+    end
+
+    def split_line(line)
+       return line.split(" ")
+    end
+
+    # TODO: come back to this later
+    def handle_string(line, line_arr)
+        name = line_arr[1]
+    end
+end
+
 
 # do the stuff
 
 def interpret(filename)
     first_pass = FirstPass.new(ARGV[0])
-    @lines, @heres = first_pass.process_file
-    p @lines
+    lines, heres = first_pass.process_file
+    p lines
     puts
     puts
-    p @heres
-    # find where all the here statemens are, and what
+    p heres
+    # find where all the here statements are, and what
     # their names are
     # TODO: start interpreting the file
+
+    # program counter
+    $prog_count = 0
+    while $prog_count < lines.length
+
+        prog_count += 1
+    end
+
 end
 
 
@@ -24,3 +56,4 @@ if __FILE__ == $0
 
     interpret(ARGV[0])
 end
+
